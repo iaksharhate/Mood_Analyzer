5d8271c4222ac31947f24175b93aaec47721c9e2
iaksharhate <iaksharhate@gmail.com>
2022-05-14 15:55:14 +0530

    Added: Ability to analyze mood and respond either Happy or Sad.

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -3,7 +3,10 @@ package com.bridgelabz;
 public class MoodAnalyzer {
 
     public String analyzeMood(String message) {
-
+        if (message.contains("Sad"))
+            return "SAD";
+        else if (message.contains("Happy"))
+            return  "HAPPY";
         return null;
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -4,5 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String result = moodAnalyzer.analyzeMood("User is Sad");
+        Assertions.assertEquals("SAD", result);
+    }
 
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String result = moodAnalyzer.analyzeMood("User is Happy");
+        Assertions.assertEquals("HAPPY", result);
+    }
 }
